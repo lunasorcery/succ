c19b0d6aae095906bb6c9022a7e9482450803c28
Luna Sorcery <luna@moonbase.lgbt>
2019-12-14 21:28:38 +0000

    make succ possibly the first c++23-aware compiler

--- main.cpp
+++ main.cpp
@@ -148,6 +148,9 @@ static void handleStdFlag(const std::string& flag)
 		} else if (version == "c++98" || version == "c++03") {
 			fprintf(stderr, "%swarning:%s Bring yourself into the modern day, gramps. [%s]\n", gColorBrightMagenta, gColorReset, flag.c_str());
 			return;
+		} else if (version == "c++23") {
+			fprintf(stderr, "%swarning:%s Look, I want epochs as much as you do, but be patient. We're not there yet. [%s]\n", gColorBrightMagenta, gColorReset, flag.c_str());
+			return;
 		}
 		else if (version == "c++11" || version == "c++14" || version == "c++17" || version == "c++2a") {
 			// do nothing
